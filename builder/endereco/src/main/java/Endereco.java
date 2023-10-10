000d3b87f5962b0ac6295588450ce7cddb72256a
Guilherme-Abraao <abraaosilva.guilherme@gmail.com>
2023-10-10 10:56:01 -0300

    Atividade 1 semana 3

--- builder/endereco/src/main/java/Endereco.java
+++ builder/endereco/src/main/java/Endereco.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Endereco {
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class Endereco {
+    private String rua;
+    private String setor;
+    private String cep;
+    private String numero;
+    private String cidade;
+    private String estado;
+    private String bloco;
+    private String quadra;
+    private String complemento;
 }
